4e80a97ae03c8beea466d6a719adc32b6c64e23e
Anuvrat <anuvrats06@gmail.com>
2022-04-15 17:17:19 +0530

    Implemented more OPcodes

--- README.md
+++ README.md
@@ -1,12 +1,23 @@
 [CHIP-8](https://en.wikipedia.org/wiki/CHIP-8) sub implementation in Rust
 
-
 ### OP codes
-|- [x]|Opcode|Type|Explaination|
-|-|-|-|-|
-|-[ ]|`0NNN`|`Call`|Calls machine code routine (RCA 1802 for COSMAC VIP) at address NNN. Not necessary for most ROMs.| 
-|- [x]|`00EE`|`Flow`|Returns from a subroutine.|
-|- [x]|`2NNN`|`Flow`|Calls subroutine at NNN.|
-|- [x]|`8XY4`|`Math`|Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there is not.|
 
-
+| ✓   | Opcode | Type      | Function            | Test | Explaination                                                                             |
+| --- | ------ | --------- | ------------------- | ---- | ---------------------------------------------------------------------------------------- |
+| ✗   | `0NNN` | `Call`    |                     | ✗    | Calls machine code routine at address NNN.                                               |
+| ✗   | `00E0` | `Display` |                     | ✗    | Clears the screen.                                                                       |
+| ✓   | `00EE` | `Flow`    | `ret()`             | ✗    | Returns from a subroutine.                                                               |
+| ✓   | `1NNN` | `Flow`    | `jump()`            | ✗    | Jumps to address NNN.                                                                    |
+| ✓   | `2NNN` | `Flow`    | `call()`            | ✗    | Calls subroutine at NNN.                                                                 |
+| ✓   | `3XKK` | `Cond`    | `skip_eq`           | ✓    | Skips the next instruction if VX equals KK.                                              |
+| ✓   | `4XKK` | `Cond`    | `skip_ne`           | ✓    | Skips the next instruction if VX does not equal KK                                       |
+| ✓   | `5XY0` | `Cond`    | `skip_eq`           | ✓    | Skips the next instruction if VX equals VY.                                              |
+| ✓   | `6XKK` | `Const`   | `set()`             | ✓    | Sets VX to NN.                                                                           |
+| ✓   | `7XKK` | `Const`   | `add()`             | ✓    | Adds NN to VX.                                                                           |
+| ✓   | `8XY0` | `Assig`   | `set()`             | ✓    | Sets VX to the value of VY.                                                              |
+| ✓   | `8XY1` | `BitOp`   | `or_bitwise_set()`  | ✓    | Sets VX to VX or VY                                                                      |
+| ✓   | `8XY2` | `BitOP`   | `add_bitwise_set()` | ✓    | Sets VX to VX and VY.                                                                    |
+| ✓   | `8XY3` | `BitOp`   | `xor_bitwise_set()` | ✓    | Sets VX to VX xor VY.                                                                    |
+| ✓   | `8XY4` | `Math`    | `add_xy()`          | ✓    | Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there is not.          |
+| ✓   | `8XY5` | `Math`    | `sub_xy()`          | ✓    | VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there is not. |
+| ✓   | `9XY0` | `Cond`    | `skip_ne()`         | ✓    | VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there is not. |
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ pub type OPCODETYPE = u16;
 pub struct CPU {
     // current_operation: OPCODETYPE,
     registers: [u8; 16],
+    // 16 bit register for memory address
+    // register: OPCODETYPE,
     // position in memory
     position_in_memory: usize, // usize as it can be used for indexing, spec is u16
     memory: [u8; 4096],
@@ -30,33 +32,44 @@ impl CPU {
             let d = ((opcode & 0x000F) >> 0) as u8;
 
             let nnn = opcode & 0x0FFF;
-            let _kk = (opcode & 0x00FF) as u8;
+            let kk = (opcode & 0x00FF) as u8;
 
             match (c, x, y, d) {
-                (0, 0, 0, 0) => return,
-                (0, 0, 0xE, 0xE) => self.ret(),
+                (0x0, 0x0, 0x0, 0x0) => return,
+                (0x0, 0x0, 0xE, 0x0) => { /*Not Implemented */ }
+                (0x0, 0x0, 0xE, 0xE) => self.ret(),
+                (0x1, _, _, _) => self.jump(nnn),
                 (0x2, _, _, _) => self.call(nnn),
+                (0x3, _, _, _) => self.kk_skip_eq(x, kk),
+                (0x4, _, _, _) => self.kk_skip_ne(x, kk),
+                (0x5, _, _, 0x0) => self.xy_skip_eq(x, y),
+                (0x6, _, _, _) => self.set(x, kk),
+                (0x7, _, _, _) => self.add(x, kk),
+                (0x8, _, _, 0x0) => self.set(x, self.registers[y as usize]),
+                (0x8, _, _, 0x1) => self.or_bitwise_set(x, y),
+                (0x8, _, _, 0x2) => self.and_bitwise_set(x, y),
+                (0x8, _, _, 0x3) => self.xor_bitwise_set(x, y),
                 (0x8, _, _, 0x4) => self.add_xy(x, y),
+                (0x8, _, _, 0x5) => self.sub_xy(x, y),
+                (0x9, _, _, 0x0) => self.xy_skip_ne(x, y),
 
                 _ => todo!("opcode {:04x}", opcode),
             }
         }
     }
 
-    pub fn add_xy(&mut self, x: u8, y: u8) {
-        let arg1 = self.registers[x as usize];
-        let arg2 = self.registers[y as usize];
-
-        let (val, overflow) = arg1.overflowing_add(arg2);
-        self.registers[x as usize] = val;
-
-        if overflow {
-            self.registers[0xF] = 1;
-        } else {
-            self.registers[0xF] = 0;
+    pub fn ret(&mut self) {
+        if self.stack_pointer == 0 {
+            panic!("Stack underflow");
         }
-    }
 
+        self.stack_pointer -= 1;
+        let addr = self.stack[self.stack_pointer];
+        self.position_in_memory = addr as usize;
+    }
+    pub fn jump(&mut self, nnn: OPCODETYPE) {
+        self.position_in_memory = nnn as usize;
+    }
     pub fn call(&mut self, addr: OPCODETYPE) {
         let sp = self.stack_pointer;
         let stack = &mut self.stack;
@@ -70,30 +83,248 @@ impl CPU {
         self.position_in_memory = addr as usize;
     }
 
-    pub fn ret(&mut self) {
-        if self.stack_pointer == 0 {
-            panic!("Stack underflow");
+    pub fn xy_skip_eq(&mut self, x: u8, y: u8) {
+        let vx = self.registers[x as usize];
+        let vy = self.registers[y as usize];
+        if vx == vy {
+            self.position_in_memory += 2;
         }
+    }
+    pub fn xy_skip_ne(&mut self, x: u8, y: u8) {
+        let vx = self.registers[x as usize];
+        let vy = self.registers[y as usize];
+        if vx != vy {
+            self.position_in_memory += 2;
+        }
+    }
+    pub fn kk_skip_eq(&mut self, x: u8, kk: u8) {
+        let vx = self.registers[x as usize];
+        if vx == kk {
+            self.position_in_memory += 2;
+        }
+    }
+    pub fn kk_skip_ne(&mut self, x: u8, kk: u8) {
+        let vx = self.registers[x as usize];
+        if vx != kk {
+            self.position_in_memory += 2;
+        }
+    }
+    pub fn set(&mut self, vx: u8, kk_vy: u8) {
+        self.registers[vx as usize] = kk_vy;
+    }
+    pub fn add(&mut self, vx: u8, kk_vy: u8) {
+        self.registers[vx as usize] += kk_vy;
+    }
 
-        self.stack_pointer -= 1;
-        let addr = self.stack[self.stack_pointer];
-        self.position_in_memory = addr as usize;
+    pub fn or_bitwise_set(&mut self, x: u8, y: u8) {
+        let vx = self.registers[x as usize];
+        let vy = self.registers[y as usize];
+
+        self.registers[x as usize] = vx | vy;
+    }
+    pub fn and_bitwise_set(&mut self, x: u8, y: u8) {
+        let vx = self.registers[x as usize];
+        let vy = self.registers[y as usize];
+
+        self.registers[x as usize] = vx & vy;
+    }
+    pub fn xor_bitwise_set(&mut self, x: u8, y: u8) {
+        let vx = self.registers[x as usize];
+        let vy = self.registers[y as usize];
+
+        self.registers[x as usize] = vx ^ vy;
+    }
+    pub fn add_xy(&mut self, x: u8, y: u8) {
+        let vx = self.registers[x as usize];
+        let vy = self.registers[y as usize];
+
+        let (val, overflow) = vx.overflowing_add(vy);
+        self.registers[x as usize] = val;
+
+        if overflow {
+            self.registers[0xF] = 1;
+        } else {
+            self.registers[0xF] = 0;
+        }
+    }
+
+    pub fn sub_xy(&mut self, x: u8, y: u8) {
+        let vx = self.registers[x as usize];
+        let vy = self.registers[y as usize];
+
+        let (val, overflow) = vx.overflowing_sub(vy);
+        self.registers[x as usize] = val;
+
+        if overflow {
+            self.registers[0xF] = 0
+        } else {
+            self.registers[0xF] = 1
+        }
     }
 }
 #[cfg(test)]
 mod tests {
     use crate::CPU;
 
-    #[test]
-    fn basic_addition() {
-        let mut cpu = CPU {
+    fn new_cpu() -> CPU {
+        CPU {
             // current_operation: 0,
             registers: [0; 16],
             position_in_memory: 0,
             memory: [0; 4096],
             stack: [0; 16],
             stack_pointer: 0,
-        };
+        }
+    }
+    #[test]
+    fn x3xkk() {
+        let mut cpu = new_cpu();
+        cpu.registers[0] = 5;
+        cpu.registers[1] = 6;
+        cpu.registers[2] = 7;
+        cpu.registers[3] = 8;
+
+        let mem = &mut cpu.memory;
+
+        mem[0x000] = 0x30;
+        mem[0x001] = 0x05;
+        mem[0x002] = 0x80;
+        mem[0x003] = 0x24;
+        mem[0x004] = 0x80;
+        mem[0x005] = 0x34;
+
+        cpu.run();
+        // 5 + 7 + 8 = 13 // OPCODE 0x3005 skips next instruction as vx == kk
+        assert_eq!(cpu.registers[0], 13);
+    }
+    #[test]
+    fn x4xkk() {
+        let mut cpu = new_cpu();
+        cpu.registers[0] = 5;
+        cpu.registers[1] = 6;
+        cpu.registers[2] = 7;
+        cpu.registers[3] = 8;
+
+        let mem = &mut cpu.memory;
+
+        mem[0x000] = 0x40;
+        mem[0x001] = 0x10;
+        mem[0x002] = 0x80;
+        mem[0x003] = 0x24;
+        mem[0x004] = 0x80;
+        mem[0x005] = 0x34;
+
+        cpu.run();
+        // 5 + 7 + 8 = 13 // OPCODE 0x4010 skips next instruction as vx != kk
+        assert_eq!(cpu.registers[0], 13);
+    }
+    #[test]
+    fn x5xy0() {
+        let mut cpu = new_cpu();
+        cpu.registers[0] = 12;
+        cpu.registers[1] = 12;
+
+        let mem = &mut cpu.memory;
+
+        mem[0x000] = 0x50;
+        mem[0x001] = 0x10;
+        mem[0x002] = 0x80;
+        mem[0x003] = 0x14;
+
+        cpu.run();
+        // skip vx = vy
+        assert_eq!(cpu.registers[0], 0x000C);
+    }
+    #[test]
+    fn x6xkk() {
+        let mut cpu = new_cpu();
+
+        let mem = &mut cpu.memory;
+        mem[0x000] = 0x60;
+        mem[0x001] = 0x10; // kk = 16
+
+        cpu.run();
+        // vx = kk
+        assert_eq!(cpu.registers[0], 16);
+    }
+    #[test]
+    fn x7xkk() {
+        let mut cpu = new_cpu();
+        cpu.registers[0] = 10;
+
+        let mem = &mut cpu.memory;
+
+        mem[0x000] = 0x70;
+        mem[0x001] = 0x10; // kk = 16
+
+        cpu.run();
+        // vx = vy
+        assert_eq!(cpu.registers[0], 26);
+    }
+    #[test]
+    fn x8xy0() {
+        let mut cpu = new_cpu();
+        cpu.registers[0] = 9;
+        cpu.registers[1] = 99;
+
+        let mem = &mut cpu.memory;
+
+        mem[0x000] = 0x80;
+        mem[0x001] = 0x10;
+
+        cpu.run();
+        // vx = vy
+        assert_eq!(cpu.registers[0], cpu.registers[1]);
+    }
+    #[test]
+    fn x8xy1() {
+        let mut cpu = new_cpu();
+        cpu.registers[0] = 9;
+        cpu.registers[1] = 99;
+
+        let mem = &mut cpu.memory;
+
+        mem[0x000] = 0x80;
+        mem[0x001] = 0x11;
+
+        cpu.run();
+        // BITWSIE OR
+        assert_eq!(cpu.registers[0], 107);
+    }
+
+    #[test]
+    fn x8xy2() {
+        let mut cpu = new_cpu();
+        cpu.registers[0] = 9;
+        cpu.registers[1] = 99;
+
+        let mem = &mut cpu.memory;
+
+        mem[0x000] = 0x80;
+        mem[0x001] = 0x12;
+
+        cpu.run();
+        // BITWISE AND
+        assert_eq!(cpu.registers[0], 1);
+    }
+    #[test]
+    fn x8xy3() {
+        let mut cpu = new_cpu();
+        cpu.registers[0] = 9;
+        cpu.registers[1] = 99;
+
+        let mem = &mut cpu.memory;
+
+        mem[0x000] = 0x80;
+        mem[0x001] = 0x13;
+
+        cpu.run();
+        // BITWISE XOR
+        assert_eq!(cpu.registers[0], 106);
+    }
+    #[test]
+    fn x8xy4() {
+        let mut cpu = new_cpu();
 
         // opcode `0x8014`
         // 8 signifies involment of two registers.
@@ -115,19 +346,49 @@ mod tests {
         mem[5] = 0x34; // register three low nibble ...
 
         cpu.run();
-
+        // "5 + 10 + 10 + 10 = 35"
         assert_eq!(cpu.registers[0], 35);
     }
+    #[test]
+    fn x8xy5() {
+        let mut cpu = new_cpu();
+        cpu.registers[0] = 30;
+        cpu.registers[1] = 5;
+        cpu.registers[2] = 5;
+        cpu.registers[3] = 5;
 
+        let mem = &mut cpu.memory;
+        mem[0] = 0x80;
+        mem[1] = 0x15;
+        mem[2] = 0x80;
+        mem[3] = 0x25;
+        mem[4] = 0x80;
+        mem[5] = 0x35;
+
+        cpu.run();
+        // 30 - 5 - 5 - 5 = 15
+        assert_eq!(cpu.registers[0], 15);
+    }
+    #[test]
+    fn x9xy0() {
+        let mut cpu = new_cpu();
+        cpu.registers[0] = 11;
+        cpu.registers[1] = 12;
+
+        let mem = &mut cpu.memory;
+
+        mem[0x000] = 0x90;
+        mem[0x001] = 0x10;
+        mem[0x002] = 0x80;
+        mem[0x003] = 0x14;
+
+        cpu.run();
+        // skip if vx != vy
+        assert_eq!(cpu.registers[0],11);
+    }
     #[test]
     fn addition_and_multiplication() {
-        let mut cpu = CPU {
-            registers: [0; 16],
-            position_in_memory: 0,
-            memory: [0; 4096],
-            stack: [0; 16],
-            stack_pointer: 0,
-        };
+        let mut cpu = new_cpu();
 
         cpu.registers[0] = 5;
         cpu.registers[1] = 10;
@@ -148,7 +409,7 @@ mod tests {
         mem[0x105] = 0xEE; // ..
 
         cpu.run();
-
+        // 5 + (10 * 2) + (10 * 2) = 45
         assert_eq!(cpu.registers[0], 45);
     }
 }
